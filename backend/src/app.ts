d63b82433dcaa41b1470b89692f65c1ef51d6f0e
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-04-15 00:46:13 +0200

    add route for auth/register

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { routerComps } from "./routes/competitions";
 import { routerLogIn } from "./routes/login";
+import { authRouter } from "./routes/auth";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 //routes
 app.use('/api/v1/competitions', routerComps);
-app.use('/api/v1/login', routerLogIn )
+app.use('/api/v1/login', routerLogIn);
+app.use('/api/v1/auth', authRouter);
 
 app.use((req, res) => {
     res.status(404).json({
