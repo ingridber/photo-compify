c781082d91502b7da1beaab8d3fe071306735541
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-04-15 00:42:25 +0200

    add config for db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import { app } from "./app";
+import { connectDb } from "./config/db";
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-});
+async function start() {
+    await connectDb();
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`);
+    });
+}
+
+start();
