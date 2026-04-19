a4433c528236ff07cc0ccf505004ea8a3c9911ef
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-04-19 23:44:14 +0200

    put route for searchCompetitions function

--- backend/src/routes/competitions.ts
+++ backend/src/routes/competitions.ts
@@ -1,10 +1,11 @@
-import { getAllCompetitions, getCompetitionById, createCompetition, updateCompetition, deleteCompetition } from "../controllers/competitionsController";
+import { getAllCompetitions, getCompetitionById, createCompetition, updateCompetition, deleteCompetition, searchCompetitions } from "../controllers/competitionsController";
 import express from "express";
 
 const routerComps = express.Router()
 
 routerComps.get('/', getAllCompetitions);
 routerComps.get('/:id', getCompetitionById);
+routerComps.get("/competitions", searchCompetitions);
 routerComps.post('/', createCompetition);
 routerComps.patch('/:id', updateCompetition);
 routerComps.delete('/:id', deleteCompetition);
