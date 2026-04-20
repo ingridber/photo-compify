c5586430f4afc1e233502d3db805041971f4a4af
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-04-20 14:31:19 +0200

    api call to login

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,18 @@
+// ---------- LOGIN ----------
+
+export async function login(email: string, password: string) {
+    const res = await fetch("http://localhost:3000/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username: email, password: password})
+    });
+
+    if(!res.ok) {
+        throw new Error('Login failed');
+    };
+
+    return res.json();
+    
+};
