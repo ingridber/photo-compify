d7172dd70b7f4b03769a63ab3b9afac2047927d7
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-04-20 13:00:37 +0200

    update model to follow interface structure

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,10 +2,14 @@ import { Schema, model } from "mongoose";
 import { InterfaceUser } from "../types/index";
 
 const userSchema = new Schema<InterfaceUser>({
+    name: { type: String, required: false, maxlength: 80 },
     username: { type: String, required: true, unique: true, lowercase: true, minlength: 3, maxlength: 80 },
+    email: { type: String, required: false, lowercase: true, maxlength: 80 },
     password: { type: String, required: true, minlength: 8, maxlength: 128 },
-    loginAttempts: {type: Number, default: 0},
-    lockUntil: {type: Date},
+    profilePicture: { type: String, required: false },
+    warnings: { type: Number, default: 0 },
+    loginAttempts: { type: Number, default: 0 },
+    lockUntil: { type: Date },
 });
 
 export const User = model<InterfaceUser>("User", userSchema);
