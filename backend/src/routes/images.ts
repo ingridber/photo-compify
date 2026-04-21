86a6ae7b8b1d67faf5ec4e1ed7ad110032a24d8f
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2026-04-21 15:25:17 +0200

    Update image routes for upload and delete endpoints

--- backend/src/routes/images.ts
+++ backend/src/routes/images.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { getAllImages, getImageById, createImage,deleteImage,updateImage } from "../controllers/imageController";
 import { upload } from "../middleware/uploadMiddleware";
+import { testSupabase } from "../controllers/imageController";
 
 const router = express.Router();
 
 router.get("/", getAllImages);
+router.get("/test-supabase", testSupabase);
 router.get("/:id", getImageById);
 router.post("/", upload.single("image"), createImage);
 router.delete("/:id", deleteImage);
