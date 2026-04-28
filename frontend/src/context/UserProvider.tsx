13cb9a3ee1d8f384dfbc115037c92949064c0817
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-04-28 14:27:24 +0200

    update context to include profile pic

--- frontend/src/context/UserProvider.tsx
+++ frontend/src/context/UserProvider.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import type { ReactNode } from "react";
 import { UserContext } from "./UserContext";
+import type { User } from "../types/user";
 
 export function UserProvider({children}: {children: ReactNode}) {
-    const [user, setUser] = useState('');
+    const [user, setUser] = useState<User | null>(null);
 
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider 
+            value={{user, setUser}}>
             {children}
         </UserContext.Provider>
     );
