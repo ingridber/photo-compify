7e2af3bf29b6927f3d2808a8298a6bb6b048ce17
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-04-29 09:55:42 +0200

    handle Logout and confirm logout ux

--- frontend/src/pages/ManageAccount.tsx
+++ frontend/src/pages/ManageAccount.tsx
@@ -1,18 +1,45 @@
 import mixins from "../styles/mixins.module.css";
 import styles from "../styles/manage-account.module.css";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useUser } from "../hooks/useUser";
-import { useNavigate } from "react-router";
+import { useState } from "react";
 
 import { DisplayProfilePicture } from "../components/display-profile-picture/DisplayProfilePicture";
+import { logout } from "../services/api";
 
 export function ManageAccount() {
-
-    const {user} = useUser();
     const navigate = useNavigate();
+    const {user, setUser} = useUser();
+    const [confirmSignOut, setConfirmSignOut] = useState(false);
+    const [message, setMessage] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const handleSignOut = async () => {
+        try {
+            await logout();
+
+            setMessage(`User: ${user?.username} succesfully logged out. Redirecting...`);
+            setRedirect(true);
+            setUser(null);
+            setTimeout(() => {
+                navigate("/");
+            }, 2000);
+        } catch (err: unknown) {
+            if(err instanceof Error) {
+                setMessage(err.message)
+            } else {
+                setMessage('Something went wrong')
+            }
+        };
+    };
 
     return(
         <div className={mixins.sectionContainer}>
+
+            {redirect 
+            ? <p>{message}</p> 
+            : <>
+                
             {/* BACK BUTTON */}
             <button 
                 onClick={()=> navigate(-1)}
@@ -30,7 +57,6 @@ export function ManageAccount() {
                 {user?.profilePicture ? "Change " : "Upload "}Profile Picture
             </Link>
 
-
             {/* UPDATE BUTTONS */}
             <Link to="change-username" className={styles.changePageBtn}>
                 Change Username
@@ -40,20 +66,36 @@ export function ManageAccount() {
                 Change Password
             </Link>
 
-
             {/* SIGN OUT */}
-
             <article>
                 <p className={`${styles.signOutTitle} ${styles.space}`}>
-                    Need a break?</p>
-                <Link to="sign-out" className={styles.signOut}>
-                    Sign Out
-                </Link>
+                    {confirmSignOut ? "Are you sure?" : "Need a break?"}
+                </p>
+
+                {!confirmSignOut ? (
+                    <button
+                        onClick={() => setConfirmSignOut(true)}
+                        className={styles.signOut}>
+                            Sign Out
+                    </button>
+                ):(
+                    <div className={styles.confirmBtnContainer
+                    }>
+                        <button
+                            onClick={handleSignOut}
+                            className={styles.confirmBtnYes}>
+                                Yes
+                        </button>
+                        <button
+                            onClick={() => setConfirmSignOut(false)}
+                            className={styles.confirmBtnNo}>
+                                No
+                        </button>
+                    </div>
+                )}
             </article>
 
-
             {/* DELETE ACCOUNT  */}
-
             <article>
                 <p className={styles.deleteAccountTitle}>
                     Wanna break up? :-(</p>
@@ -61,9 +103,8 @@ export function ManageAccount() {
                     Delete Account
                 </Link>
             </article>
-
-            
-
+            </>
+            }
         </div>    
     )
-}
+};
