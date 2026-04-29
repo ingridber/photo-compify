86f3d177d77826abe147f08d3ff007a8ea6ac441
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-04-29 11:23:30 +0200

    moved register user link to sign in form pga lat

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,20 +1,10 @@
 import { SignInForm } from "../components/SignInForm";
-import { Link } from "react-router";
-import styles from "../styles/sign-in.module.css";
+// import styles from "../styles/sign-in.module.css";
 
 export function SignIn() {
     return (
         <div>
-
             <SignInForm/>
-
-
-            <p className={styles.registerTitle}>Not a member?</p>
-            <Link to="/register" className={styles.register}>
-                Create an account
-            </Link>
-
-
         </div>
     )
 }
