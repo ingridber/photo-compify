20a47e7bec057ca13a28705d588fac67b44aed45
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-04-29 13:26:01 +0200

    change sign in to btn manage account when logged in

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,28 +1,26 @@
 import { Link } from "react-router";
+import { useUser } from "../hooks/useUser";
 
 export function Home() {
+
+    const {user} = useUser();
+
     return (
         <div>
             <nav>
-                <Link to="/login">
-                    <button>Sign In</button>
-                </Link>
+                {!user ? (
+                    <Link to="/login">
+                        <button>Sign In</button>
+                    </Link>
+                ) : (
+                    <Link to="/manage-account">
+                        <button>Manage Account</button>
+                    </Link>
+                )}
 
-                {/* <Link to="/register">
-                    <button>Register user</button>
-                </Link> */}
-
-                <Link to="/manage-account">
-                    <button>Manage Account</button>
-                </Link>
-                
                 <Link to="/competitions">
                     <button>Competitions</button>
                 </Link>
-                
-                {/* <Link to="/edit-profile">
-                    <button>Edit Profile</button>
-                </Link> */}
             </nav>
 
             <h1>Welcome</h1>
