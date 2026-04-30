6ff81c83fc873c362b0e5e4e118cfc4e6f2e60a7
Delzarkafashi <145854766+Delzarkafashi@users.noreply.github.com>
2026-04-30 09:10:55 +0200

    clean up

--- frontend/src/services/imageApi.ts
+++ frontend/src/services/imageApi.ts
@@ -1,11 +1,6 @@
-export async function getImages() {
-  const res = await fetch("http://localhost:3000/api/v1/images");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch images");
-  }
-
-  const data = await res.json();
-
-  return data.data;
+export async function uploadImage(formData: FormData) {
+  return fetch("http://localhost:3000/api/v1/images", {
+    method: "POST",
+    body: formData
+  });
 }
