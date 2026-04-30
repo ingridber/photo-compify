5fbd8ec9963bf6dd569d1454624bd0486e75c721
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-04-30 11:35:10 +0200

    adding route to change-picture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import { useEffect } from 'react';
 import { useUser } from './hooks/useUser';
 import { getCurrentUser } from './services/api';
 
+
 function App() {
 
   const{setUser} = useUser();
