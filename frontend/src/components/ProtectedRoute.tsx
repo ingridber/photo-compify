ec17bfe9b1c2a1bc3831ebcf9eb36cfda12c9c1a
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-05-06 12:59:19 +0200

    add navigate back to previous page

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,12 +1,13 @@
-import { Navigate } from "react-router";
+import { Navigate, useLocation } from "react-router";
 import { useUser } from "../hooks/useUser";
 import type { ReactNode } from "react";
 
 export function ProtectedRoute({children}: {children: ReactNode}) {
     const {user} = useUser();
+    const location = useLocation();
 
     if(!user) {
-        return <Navigate to="/" replace/>;
+        return <Navigate to="/login" state={{ from: location }} replace/>;
     }
 
     return children;
