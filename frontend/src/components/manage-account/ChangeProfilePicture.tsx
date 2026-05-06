f27f81f16f9854b74c5cf8de3587f737022a8e32
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-05-06 13:05:03 +0200

    add button to remember delete pic

--- frontend/src/components/manage-account/ChangeProfilePicture.tsx
+++ frontend/src/components/manage-account/ChangeProfilePicture.tsx
@@ -1,30 +1,24 @@
 import mixins from "../../styles/mixins.module.css";
 import styles from "../../styles/manage-account.module.css";
-
 import ImageUploadForm from "../images/ImageUploadForm";
 import { DisplayProfilePicture } from "../display-profile-picture/DisplayProfilePicture";
 import { useUser } from "../../hooks/useUser";
 import { useNavigate } from "react-router";
 import { useState } from "react";
 
-
 export function ChangeProfilePicture(){
-
     const navigate = useNavigate();
     const { user} = useUser();
     const [openModal, setOpenModal] = useState(false);
 
-
     return (
-
         <div className={mixins.sectionContainer}>
         {/* BACK BUTTON */}
             <button onClick={() => navigate(-1)} className={mixins.backBtn}>
                 <img
                 src="/arrow-left.svg"
                 alt="icon of arrow pointing left"
-                className={mixins.backBtnIcon}
-                />
+                className={mixins.backBtnIcon}/>
             </button>
 
         {/* USER NAME & PROFILE PICTURE */}
@@ -33,24 +27,26 @@ export function ChangeProfilePicture(){
                 <DisplayProfilePicture src={user?.profilePicture} />
             </div>
 
-
             <button 
                 className={styles.changePageBtn}
                 onClick={() => setOpenModal(true)}>
-                    {user?.profilePicture ? "Change " : "Upload "}Profile Picture
+                    {user?.profilePicture ? "Change " : "Upload "} Picture
+            </button>
+
+            <button 
+                className={styles.changePageBtn}>
+                    (fix) Delete Picture
             </button>
 
             {openModal && (
                 <div className={styles.modalOverlay} onClick={() => setOpenModal(false)}>
                     <div 
                     className={styles.modalContent} 
-                    onClick={(e) => e.stopPropagation()}
-                    >
+                    onClick={(e) => e.stopPropagation()}>
                         <button 
                             className={styles.closeBtn}
-                            onClick={() => setOpenModal(false)}
-                        >
-                            ✕
+                            onClick={() => setOpenModal(false)}>
+                                ✕
                         </button>
 
                         <ImageUploadForm 
@@ -58,10 +54,7 @@ export function ChangeProfilePicture(){
                             onUploadSuccess={() => setOpenModal(false)}/>
                     </div>
                 </div>
-
             )}
-
-
         </div>
-    )
-}
+    );
+};
