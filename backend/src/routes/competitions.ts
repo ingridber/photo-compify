aff3af27b676accd102362d917c2c1b91662ed26
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-05-06 18:24:39 +0200

    update routes to reflect how we actually add submissions

--- backend/src/routes/competitions.ts
+++ backend/src/routes/competitions.ts
@@ -1,12 +1,12 @@
 import { getAllCompetitions, getCompetitionById, createCompetition, updateCompetition, deleteCompetition } from "../controllers/competitionsController";
-import {authenticateToken} from "../middleware/auth";
+import {authenticateToken, extractUser} from "../middleware/auth";
 import express from "express";
 import { upload } from "../middleware/uploadMiddleware";
 
 const routerComps = express.Router()
 
 routerComps.get('/', getAllCompetitions);
-routerComps.get('/:id', getCompetitionById);
+routerComps.get('/:id', extractUser, getCompetitionById);
 routerComps.post('/', authenticateToken, upload.single("logoBaner"), createCompetition);
 routerComps.patch('/:id', authenticateToken,  updateCompetition);
 routerComps.delete('/:id', authenticateToken,  deleteCompetition);
--- backend/src/routes/submissions.ts
+++ backend/src/routes/submissions.ts
@@ -1,14 +1,13 @@
 import { getSubmission, createSubmission, deleteSubmission, updateSubmission, voteOnSubmission, removeVoteFromSubmission } from "../controllers/submissionsController";
 import {authenticateToken} from "../middleware/auth";
 import express from "express";
-import { upload } from "../middleware/uploadMiddleware";
 
 const submissionRouter = express.Router({ mergeParams: true });
 
-submissionRouter.post('/', authenticateToken, upload.single("image"), createSubmission);
+submissionRouter.post('/', authenticateToken, createSubmission);
 
 submissionRouter.get('/:id', getSubmission);
-submissionRouter.patch('/:id', authenticateToken, upload.single("image"), updateSubmission);
+submissionRouter.patch('/:id', authenticateToken, updateSubmission);
 submissionRouter.delete('/:id', authenticateToken, deleteSubmission);
 submissionRouter.post('/:id/vote', authenticateToken, voteOnSubmission);
 submissionRouter.delete('/:id/vote', authenticateToken, removeVoteFromSubmission);
