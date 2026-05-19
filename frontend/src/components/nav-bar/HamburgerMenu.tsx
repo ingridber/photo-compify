be907ecfa0900da0b518f1931a546644b1eaa736
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-05-19 13:18:18 +0200

    new styling

--- frontend/src/components/nav-bar/HamburgerMenu.tsx
+++ frontend/src/components/nav-bar/HamburgerMenu.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, NavLink } from "react-router";
 import styles from "./HamburgerMenu.module.css";
 import { useTheme } from "../../hooks/useTheme";
 import { useUser } from "../../hooks/useUser";
+import { DisplayLogo } from "../display-profile-picture/DisplayProfilePicture";
+
 
 export default function HamburgerMenu() {
   const [open, setOpen] = useState(false);
@@ -17,14 +19,23 @@ export default function HamburgerMenu() {
 
   return (
     <nav className={styles.navbar}>
+
+    <div className={styles.logoContainer} onClick={() => goTo("/")}>
+       <DisplayLogo text={false} />
+    </div>
+   
+
       {/* HAMBURGER */}
       <button
         className={styles.hamburger}
         onClick={() => setOpen(prev => !prev)}
       >
-        <span />
-        <span />
-        <span />
+        {open ? (
+          // <p className={styles.backBtnIcon}>✕</p>
+          <img src="/close.svg" alt="" className={styles.hamburgerIcon}/>
+        ) : (
+            <img src="/menu.svg" alt="" className={styles.hamburgerIcon}/>
+        )}
       </button>
 
       
@@ -34,8 +45,8 @@ export default function HamburgerMenu() {
   <div className={styles.dropdown}>
     {!user ? (
       <>
-        <button onClick={() => goTo("/login")}>Sign in</button>
-        <button onClick={toggleTheme}>
+        <button onClick={() => goTo("/login")} className={styles.menuBtn}>Sign in</button>
+        <button onClick={toggleTheme} className={styles.menuBtn}>
           {theme === "light" ? "Dark Mode" : "Light Mode"}
         </button>
       </>
@@ -44,9 +55,9 @@ export default function HamburgerMenu() {
         <span className={styles.username}>
         Signed in as: {user?.username?.toLocaleUpperCase()}
         </span>
-        <button onClick={() => goTo("/manage-account")}>Manage account</button>
-        <button onClick={() => goTo("/competitions")}>Competitions</button>
-        <button onClick={toggleTheme}>
+        <button onClick={() => goTo("/manage-account")} className={styles.menuBtn}>Manage account</button>
+        <button onClick={() => goTo(`/competitions?search=${encodeURIComponent(user.username)}`)} className={styles.menuBtn}>My Competitions</button>
+        <button onClick={toggleTheme} className={styles.menuBtn}>
           {theme === "light" ? "Dark Mode" : "Light Mode"}
         </button>
       </>
