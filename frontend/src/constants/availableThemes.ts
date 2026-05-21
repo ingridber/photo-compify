f01a17a29abf22836b0eddfc43f253a0e98c77e4
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-05-21 17:44:27 +0200

    created file for availableThemes and made export on all available themes

---
+++ frontend/src/constants/availableThemes.ts
@@ -0,0 +1,20 @@
+const AVAILABLE_THEMES: string[] = [
+    'Portrait',
+    'Landscape',
+    'Street',
+    'Wildlife',
+    'Macro',
+    'Architecture',
+    'Nature',
+    'Travel',
+    'Minimalist',
+    'Black & White',
+    'Long Exposure',
+    'Abstract',
+    'Aerial',
+    'Night',
+    'Astrophotography',
+    'Documentary',
+];
+
+export default AVAILABLE_THEMES;
