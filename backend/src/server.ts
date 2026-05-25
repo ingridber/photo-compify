613b07c9af55900bbc50b4cfa255c93d31b5bf12
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-05-25 14:48:57 +0200

    check phases before server start

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import { app } from "./app";
 import { connectDb } from "./config/db";
+import { initCompetitionPhases } from "./utils/competitions/initCompetitionPhases";
 
 const PORT = process.env.PORT || 3000
 
 async function start() {
-    await connectDb();
+    await connectDb().then(async () => {
+        await initCompetitionPhases();
+    });
     app.listen(PORT, () => {
         console.log(`server running on port ${PORT}`);
     });
