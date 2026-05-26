e9b16fcc1201c034c40c64e64824a66c31c7597c
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-05-26 08:14:03 +0200

    added styling

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -5,13 +5,13 @@ import { useEffect, useState } from "react";
 import { getUserStats } from "../services/api";
 import { Outlet, useNavigate } from "react-router";
 import ProfileEditDetails from "../components/profile/ProfileEditDetails";
+import profileStyle from "../components/profile/profile.module.css";
+
 
 export function ProfilePage() {
-
     const { user } = useUser();
     const navigate = useNavigate();
     const [showEdit, setShowEdit] = useState(false);
-
     const [stats, setStats] = useState({
         wins: 0,
         submissions: 0,
@@ -19,51 +19,34 @@ export function ProfilePage() {
     });
 
     useEffect(() => {
-
         async function loadStats() {
-
             try {
-
                 const result = await getUserStats();
-
                 setStats(result);
-
             } catch (err) {
                 console.log(err);
             }
         }
-
         loadStats();
-
     }, []);
 
     return (
         <>
-
-
-
             {/* HEADER */}
             <section className={mixins.headerContainer}>
-
                 <p 
-                    onClick={() => navigate("/profile")}
-                    className={mixins.username}>
-                    {user ? user.username : "USER"}
+                    className={mixins.username}
+                    onClick={() => navigate("/profile")}>
+                        {user ? user.username : "USER"}
                 </p>
-
                 <div
-                    style={{
-                        width: "7rem",
-                        margin: "auto"
-                    }}
+                    className={profileStyle.pictureContainer}
                     onClick={() => navigate("/profile")}
                 >
-                    <DisplayProfilePicture
-                        src={user?.profilePicture?.url}
-                    />
+                    <DisplayProfilePicture src={user?.profilePicture?.url}/>
                 </div>
-
             </section>
+
 
             <button>
                 <img
@@ -73,78 +56,67 @@ export function ProfilePage() {
                     onClick={() => setShowEdit(!showEdit)}
                 />
             </button>
-            {/* USER CAMERA & PREFERRED THEMES*/}
 
-            <section>
 
+        {/* ----- CAMERA AND THEMES DETAILS ----- */}
+            <section className={profileStyle.profileDetailsContainer}>
                 { showEdit ? (
                     <ProfileEditDetails 
                         handleSave={() => setShowEdit(false)}/>
                 ):(
                     <>
-                        <div>
-                            <img
-                                src="/camera-detail.svg"
-                                alt="camera icon"
-                            />
-                            <p>{user?.camera? user.camera : "Not specified"}</p>
-                        </div>
-                        <div>
-                            <img
-                                src="/theme-detail.svg"
-                                alt="theme icon"
-                            />
-                            <p>{user?.themes? user.themes : "Not specified"}</p>
-                        </div>
+                    <div className={profileStyle.detailsContainer}>
+                        <img src="/camera-detail.svg" alt="camera icon" className={profileStyle.detailsIcon}/>
+                        <p className={profileStyle.detailsText}>
+                            {user?.camera? user.camera : "Not specified"}</p>
+                    </div>
+
+                    <div className={profileStyle.detailsContainer}>
+                        <img src="/theme-detail.svg" alt="theme icon" className={profileStyle.detailsIcon}/>
+
+                        {user?.themes && user.themes.length > 0 ? (
+                            user.themes.map((theme) => {
+                
+                            const safeTheme = theme ?? "Default";
+                            const themeClass = `${safeTheme
+                                .replace(/\s+/g, "")
+                                .replace(/&/g, "")}Color`;
+
+                            return (
+                                <span
+                                className={`${profileStyle.theme} ${profileStyle[themeClass]}`}
+                                key={safeTheme}
+                                >
+                                {safeTheme}
+                                </span>
+                            );
+                        })
+                        ) : (
+                            <span className={profileStyle.detailsText}>Not specified</span>
+                        )}
+                    </div>
                     </>
-                )
-
-
-
-
-                }
-               
-
-
+                )}
             </section>
 
-{/* användare får välja max tre av följande themes:
-    'Portrait',
-    'Landscape',
-    'Street',
-    'Wildlife',
-    'Macro',
-    'Architecture',
-    'Nature',
-    'Travel',
-    'Minimalist',
-    'Black & White',
-    'Long Exposure',
-    'Abstract',
-    'Aerial',
-    'Night',
-    'Astrophotography',
-    'Documentary', */}
-
-
-
-
             {/* STATS */}
-            <section>
-
-                <div>
-                    <p>Wins: {stats.wins}</p>
-                    <p
-                        onClick={() => navigate("/profile")}
-                        >Submissions: {stats.submissions}</p>
-                    <p onClick={() =>
-                        navigate("/profile/competitions")
-                    }>
-                        Competitions created:
-                        {stats.competitionsCreated}
-                    </p>
+            <section className={profileStyle.profileStatsContainer}>
+                <div 
+                    className={`${profileStyle.statContainer} ${profileStyle.competitionsContainer}`}
+                    onClick={() => navigate("/profile/competitions")}>
+                        <p className={profileStyle.statTitle}>Host</p>
+                        <p className={profileStyle.statValue}>{stats.competitionsCreated}</p>
                 </div>
-
+                 <div 
+                    className={`${profileStyle.statContainer} ${profileStyle.submissionContainer}`}
+                    onClick={() => navigate("/profile")}>
+                        <p className={profileStyle.statTitle}>Submissions</p>
+                        <p className={profileStyle.statValue}>{stats.submissions}</p>
+                </div>
+                <div className={profileStyle.statContainer}>
+                    <p className={profileStyle.statTitle}>Wins</p>
+                    <p className={profileStyle.statValue}>{stats.wins}</p>
+                </div>
             </section>
 
 
--- frontend/src/pages/PublicProfilePage.tsx
+++ frontend/src/pages/PublicProfilePage.tsx
@@ -7,60 +7,42 @@ import SubmissionCard from "../components/single-competition/SubmissionCard";
 import type {Submission, Competition } from "../types/competitions";
 import type { PublicProfile } from "../types/user";
 import { Throbber } from "../components/user-feedback/Throbber";
+import profileStyle from "../components/profile/profile.module.css";
 
 export default function PublicProfilePage() {
-
     const { username } = useParams();
     const navigate = useNavigate();
-
     const [profile, setProfile] = useState<PublicProfile | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
-
-    const [view, setView] = useState<
-        "submissions" | "competitions"
-    >("submissions");
+    const [view, setView] = useState<"submissions" | "competitions">("submissions");
 
     useEffect(() => {
-
         async function loadProfile() {
-
             try {
-
-                const res = await fetch(
-                    `http://localhost:3000/api/v1/user/${username}`
-                );
+                const res = await fetch(`http://localhost:3000/api/v1/user/${username}`);
 
                 if (!res.ok) {
                     throw new Error("Failed to fetch profile");
                 }
 
                 const data = await res.json();
-
                 setProfile(data);
-
             } catch (err) {
-
                 if (err instanceof Error) {
                     setError(err.message);
                 } else {
                     setError("Unknown error");
-    }
-
+                }
             } finally {
-
                 setLoading(false);
             }
         }
-
         loadProfile();
-
     }, [username]);
 
     if (loading) return <Throbber/>;
-
     if (error) return <p>{error}</p>;
-
     if (!profile) return <p>User not found</p>;
 
     return (
@@ -75,87 +57,83 @@ export default function PublicProfilePage() {
 
         {/* PROFILE HEADER */}
         <section className={mixins.headerContainer}>
-
             <p 
                 className={mixins.username}
-                onClick={() =>
-                        setView("submissions")
-                    }>
-                {profile.user.username}
+                onClick={() => setView("submissions")}>
+                    {profile.user.username}
             </p>
 
-            <div
-                style={{
-                    width: "7rem",
-                    margin: "auto",
-                    cursor: "pointer"
-                }}
-                onClick={() =>
-                    setView("submissions")
-                }
+            <div 
+                className={profileStyle.pictureContainer}
+                onClick={() =>setView("submissions")}
             >
-                <DisplayProfilePicture
-                    src={profile.user.profilePicture?.url}
-                />
-            </div>
-
-        </section>
-
-
-        <section>
-            <div>
-                <img
-                    src="/camera-detail.svg"
-                    alt="camera icon"
-                />
-                <p>{profile.user.camera? profile.user.camera : "Not specified"}</p>
-            </div>
-            <div>
-                <img
-                    src="/theme-detail.svg"
-                    alt="theme icon"
-                />
-                <p>{profile.user.themes? profile.user.themes : "Not specified"}</p>
+                <DisplayProfilePicture src={profile.user.profilePicture?.url}/>
             </div>
         </section>
 
+        {/* ----- CAMERA AND THEMES DETAILS ----- */}
+        <section className={profileStyle.profileDetailsContainer}>
+            <div className={profileStyle.detailsContainer}>
+                <img src="/camera-detail.svg" alt="camera icon" className={profileStyle.detailsIcon}/>
+                <p className={profileStyle.detailsText}>
+                    {profile.user.camera? profile.user.camera : "Not specified"}</p>
+            </div>
 
+            <div className={profileStyle.detailsContainer}>
+                <img src="/theme-detail.svg" alt="theme icon" className={profileStyle.detailsIcon}/>
+
+                {profile.user.themes && profile.user.themes.length > 0 ? (
+                    profile.user.themes.map((theme) => {
+          
+                    const safeTheme = theme ?? "Default";
+                    const themeClass = `${safeTheme
+                        .replace(/\s+/g, "")
+                        .replace(/&/g, "")}Color`;
+
+                    return (
+                        <span
+                        className={`${profileStyle.theme} ${profileStyle[themeClass]}`}
+                        key={safeTheme}
+                        >
+                        {safeTheme}
+                        </span>
+                    );
+                    })
+                    ) : (
+                        <span className={profileStyle.detailsText}>Not specified</span>
+                    )}
+            </div>
+        </section>
 
         {/* STATS */}
-        <section>
+        <section className={profileStyle.profileStatsContainer}>
 
-            <p>Wins: {profile.stats.wins}</p>
+            <div 
+                className={`${profileStyle.statContainer} ${profileStyle.competitionsContainer}`}
+                onClick={() =>setView("competitions")}>
+                    <p className={profileStyle.statTitle}>Host</p>
+                    <p className={profileStyle.statValue}>{profile.stats.competitionsCreated}</p>
+            </div>
+            <div 
+                className={`${profileStyle.statContainer} ${profileStyle.submissionContainer}`}
+                onClick={() =>setView("submissions")}>
+                    <p className={profileStyle.statTitle}>Submissions</p>
+                    <p className={profileStyle.statValue}>{profile.stats.submissions}</p>
+            </div>
+            <div className={profileStyle.statContainer}>
+                <p className={profileStyle.statTitle}>Wins</p>
+                <p className={profileStyle.statValue}>{profile.stats.wins}</p>
+            </div>
 
-            <p 
-            style={{cursor: "pointer"}} // TA BORT SEN !!!!!
-            onClick={() =>
-                    setView("submissions")
-                }>
-                Submissions:
-                {profile.stats.submissions}
-            </p>
-
-            <p 
-            style={{cursor: "pointer"}} // TA BORT SEN !!!!!
-            onClick={() =>
-                    setView("competitions")
-                }>
-                Competitions created:
-                {profile.stats.competitionsCreated}
-            </p>
 
         </section>
 
         {/* ---------- SUBMISSIONS ---------- */}
         {view === "submissions" ? (
-
-            <div className={mixins.profileSubmissionsgrid}>
-
+            <div className={profileStyle.profileSubmissionsgrid}>
                 {profile.submissions.length > 0 ? (
-
                     profile.submissions.map(
                         (submission: Submission) => (
-
                             <SubmissionCard
                                 key={submission._id}
                                 submission={{
@@ -175,37 +153,23 @@ export default function PublicProfilePage() {
                                     )
                                 }
                             />
-
                         )
                     )
-
                 ) : (
-
-                    <p>No visable submissions yet</p>
-
-
+                    <p className={profileStyle.noSubmissionsText}>No visable submissions yet</p>
                 )}
-
             </div>
-
         ) : (
-
-            <div>
-
+            <div className={profileStyle.ProfileCompetitions}>
                 {profile.competitions.map(
                     (competition: Competition) => (
-
                         <CompetitionsCard
                             key={competition._id}
                             competition={competition}
                         />
-
                     )
                 )}
-
             </div>
-
         )}
-
     </div>
 )}
