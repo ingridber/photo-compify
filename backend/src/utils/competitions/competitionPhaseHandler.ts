5165f74e758d484ab862d917dfe59d776d859520
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-05-27 18:00:23 +0200

    use new logic

--- backend/src/utils/competitions/competitionPhaseHandler.ts
+++ backend/src/utils/competitions/competitionPhaseHandler.ts
@@ -1,30 +1,23 @@
 import { Competition } from "../../models/Competition";
 import { CompetitionInterface } from "../../types";
-import { checkPhaseAndUpdate } from "./competitionPhaseChecker";
+import { PhaseCheckResult } from "./competitionPhaseChecker";
 import { getCompWinners } from "./getCompWinners";
 
-export async function competitionPhaseHandler(comp: CompetitionInterface) {
-    try {
-    const phase = await checkPhaseAndUpdate(comp);
-    const now = Date.now();
+export async function competitionPhaseHandler(
+    comp: CompetitionInterface,
+    { previousPhase, currentPhase }: PhaseCheckResult
+): Promise<void> {
 
-    if (phase === "submission") {
-        setTimeout(() => {
-            competitionPhaseHandler(comp);
-        }, comp.votingStartDate.getTime() - now)
+    if (previousPhase === 'submission' && currentPhase === 'voting') {
+        //notify participants voting has started
     }
 
-    if (phase === "voting") {
-        setTimeout(() => {
-            competitionPhaseHandler(comp);
-        }, comp.endDate.getTime() - now)
+    if (previousPhase === 'voting' && currentPhase === 'ended') {
+        const winners = await getCompWinners(comp);
+        await Competition.findOneAndUpdate(
+            { _id: comp._id },
+            { $set: { winners } }
+        );
+        //notify participants competition has ended
     }
-
-    if (phase === "ended") {
-        const winners = await getCompWinners(comp) //returns array with three users
-        await Competition.findOneAndUpdate({_id: comp._id}, {$set: {winners: winners}})
-    }
-    } catch (err) {
-        throw err
-    }
-};
+}
