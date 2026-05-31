87bed5fafb76d51053cfb475e05b7b9b0fca1ce5
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-05-31 18:33:29 +0200

    fixed button for Download your data, created handleExport for user data export feedback when button is pressed

--- frontend/src/pages/ManageAccount.tsx
+++ frontend/src/pages/ManageAccount.tsx
@@ -4,11 +4,40 @@ import { Link, useNavigate } from "react-router";
 import { useUser } from "../hooks/useUser";
 import { useState } from "react";
 import { DisplayProfilePicture } from "../components/display-profile-picture/DisplayProfilePicture";
+import { exportMyData } from "../services/exportMyData";
 
 export function ManageAccount() {
   const navigate = useNavigate();
-  const { user} = useUser();
+  const { user } = useUser();
   const [confirmSignOut, setConfirmSignOut] = useState(false);
+
+  const [isExporting, setIsExporting] = useState(false);
+  const [statusText, setStatusText] = useState("Download your data?");
+
+  // 
+  const handleExport = async () => {
+    try {
+      setIsExporting(true);
+      setStatusText("Preparing download...");
+
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      await exportMyData();
+
+      setStatusText("Download started!");
+
+      setTimeout(() => {
+        setStatusText("Download your data?");
+        setIsExporting(false);
+      }, 2000);
+    } catch (err) {
+      console.error(err);
+      alert("Failed to export data");
+
+      setIsExporting(false);
+      setStatusText("Download your data");
+    }
+  };
 
   return (
     <div>
@@ -23,62 +52,76 @@ export function ManageAccount() {
 
       {/* CONTENT CONTAINER */}
       <div className={mixins.contentContainer}>
+        <Link to="change-picture" className={styles.changeProfilePicLink}>
+          {user?.profilePicture ? "Change " : "Upload "}Profile Picture
+        </Link>
 
+        <Link to="change-username" className={styles.changePageLink}>
+          Change Username
+        </Link>
 
-      <Link to="change-picture" className={styles.changeProfilePicLink}>
-        {user?.profilePicture ? "Change " : "Upload "}Profile Picture
-      </Link>
+        <Link to="change-password" className={styles.changePageLink}>
+          Change Password
+        </Link>
 
-      <Link to="change-username" className={styles.changePageLink}>
-        Change Username
-      </Link>
+        {/* Download your data */}
+        <article>
+          <p className={`${styles.signOutTitle} ${styles.space}`}>
+            {statusText}
+          </p>
 
-      <Link to="change-password" className={styles.changePageLink}>
-        Change Password
-      </Link>
-
-      {/* SIGN OUT */}
-      <article>
-        <p className={`${styles.signOutTitle} ${styles.space}`}>
-          {confirmSignOut ? "Are you sure?" : "Need a break?"}
-        </p>
-
-        {!confirmSignOut ? (
           <button
-            onClick={() => setConfirmSignOut(true)}
+            onClick={handleExport}
             className={styles.signOut}
+            disabled={isExporting}
           >
-            Sign Out
+            {isExporting ? "Downloading..." : "Download"}
           </button>
-        ) : (
-          <div className={styles.confirmBtnContainer}>
+        </article>
+
+        {/* SIGN OUT */}
+        <article>
+          <p className={`${styles.signOutTitle} `}>
+            {confirmSignOut ? "Are you sure?" : "Need a break?"}
+          </p>
+
+          {!confirmSignOut ? (
             <button
-              onClick={() => navigate("/logout")}
-              className={styles.confirmBtnYes}>
+              onClick={() => setConfirmSignOut(true)}
+              className={styles.signOut}
+            >
+              Sign Out
+            </button>
+          ) : (
+            <div className={styles.confirmBtnContainer}>
+              <button
+                onClick={() => navigate("/logout")}
+                className={styles.confirmBtnYes}
+              >
                 Yes
               </button>
-            <button
-              onClick={() => setConfirmSignOut(false)}
-              className={styles.confirmBtnNo}
-            >
-              No
-            </button>
-          </div>
-        )}
-      </article>
+              <button
+                onClick={() => setConfirmSignOut(false)}
+                className={styles.confirmBtnNo}
+              >
+                No
+              </button>
+            </div>
+          )}
+        </article>
 
-      {/* DELETE ACCOUNT  */}
-      <article>
-        <p className={`${styles.deleteAccountTitle}`}> Wanna break up? :-(</p>
+        {/* DELETE ACCOUNT  */}
+        <article>
+          <p className={`${styles.deleteAccountTitle}`}> Wanna break up? :-(</p>
 
-        <button
-          onClick={() => navigate("/delete-account")}
-          className={styles.deleteAccount}>
+          <button
+            onClick={() => navigate("/delete-account")}
+            className={styles.deleteAccount}
+          >
             Delete Account
-        </button>
-      </article>
-    </div>
-
+          </button>
+        </article>
+      </div>
     </div>
   );
-};
+}
