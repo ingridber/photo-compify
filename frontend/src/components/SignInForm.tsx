cb215b84a3246c092e22d3b54e4a00515685bf10
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-06-01 12:22:59 +0200

    new style

--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -1,13 +1,8 @@
-import styles from "../styles/sign-in.module.css";
-import mixins from "../styles/mixins.module.css";
-import { useEffect, useState} from "react";
+import styles from "../styles/form.module.css";
+import { useState } from "react";
 import { login } from "../services/api";
 import { useUser } from "../hooks/useUser";
-import { useNavigate, useLocation } from "react-router";
-import { DisplayLogo } from "./display-profile-picture/DisplayProfilePicture";
-import { Link } from "react-router";
-import { Throbber } from "./user-feedback/Throbber";
-
+import { useNavigate, useLocation, Link } from "react-router";
 
 interface FormErrors {
     username?: string;
@@ -17,20 +12,17 @@ interface FormErrors {
 }
 
 export function SignInForm() {
-    const [username, setUsername] = useState(''); 
-    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
-    const [message, setMessage] = useState('');
-    const [redirect, setRedirect] = useState(false);
     const [errors, setErrors] = useState<FormErrors>({});
-
-    const {setUser} = useUser();
+    const { setUser } = useUser();
     const navigate = useNavigate();
     const location = useLocation();
 
     const from = location.state?.from?.pathname || "/";
 
-    const handleSubmit = async (e: React.SubmitEvent ) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
         const newErrors: FormErrors = {};
@@ -57,27 +49,24 @@ export function SignInForm() {
                 });
 
                 setErrors({});
-                setUsername('');
-                setPassword('');
+                setUsername("");
+                setPassword("");
                 setShowPassword(false);
 
-                setMessage(`${data.username} succesfully signed in.`);
-                setRedirect(true);
-                setTimeout(() => {
-                    navigate(from, { replace: true });
-                }, 1800);
+                navigate(from, { replace: true });
             }
 
             if (res.status === 423) {
-                setErrors({locked: data.message})
-                // "Too many failed logins, try again in 1h"
-                setPassword('');
+                setErrors({ locked: data.message });
+                setPassword("");
             }
 
             if (res.status === 401) {
-                setErrors({general: "Invalid credentials, username or password is incorrect" })
+                setErrors({
+                    general:
+                        "Invalid credentials, username or password is incorrect",
+                });
             }
-
         } catch (err) {
             setUser(null);
 
@@ -90,104 +79,81 @@ export function SignInForm() {
     };
 
     return (
-        <>
-        {redirect ? (
-            <Throbber message={message} action="Redirecting..." />
-        ) : (
-        <>
-        <section className={mixins.headerContainer}>
-            {/* BACK BUTTON */}
-            <button 
-                onClick={()=> navigate(-1)}
-                className={mixins.backBtn}>
-                <img src="/arrow-left.svg" alt="icon of arrow pointing left" className={mixins.backBtnIcon} />
-            </button>
+        <section className={styles.signInPanel}>
+            <h1 className={styles.title}>Sign In</h1>
 
-            {/* TITLE & LOGO */}
-            <p className={mixins.title}>Sign in</p>
-            <div style= {{width: "7rem", margin: "auto"}}>
-                <DisplayLogo text={true}/>
-            </div>
-        </section>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <div className={styles.field}>
+                    <label className={styles.label}>Username</label>
 
-        <section className={mixins.contentContainer}>
-            {/* LOGIN FORM  */}
-            <form onSubmit={handleSubmit}>
+                    {errors.username && (<p className={styles.error}>{errors.username}</p>)}
 
-            {/* FIELD GROUP: USERNAME */}
-            <div className={mixins.fieldGroup}>
-
-                {errors.username 
-                    ? <p className={mixins.labelForInput} style={{ color: "red" }}>{errors.username}</p>
-                    : <label htmlFor="username" className={mixins.labelForInput}>
-                    Username</label>
-                }
-                
-                <div className={mixins.inputFieldContainer}>
-                    <input 
-                        id="username"
-                        className={mixins.inputField}
+                    <input
+                        className={styles.input}
                         type="text"
                         placeholder="Username"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        required/>
+                    />
                 </div>
-            </div>
 
-            {/* FIELD GROUP: PASSWORD */}
-            <div className={mixins.fieldGroup}>
+                <div className={styles.field}>
+                    <label className={styles.label}>Password</label>
 
-                {errors.password 
-                    ? <p className={mixins.labelForInput} style={{ color: "red" }}>{errors.password}</p>
-                    : <label htmlFor="password" className={mixins.labelForInput}>
-                    Password</label>
-                }
+                    {errors.password && (<p className={styles.error}>{errors.password}</p>)}
 
-                <div className={mixins.inputFieldContainer}>
-                    <input 
-                        id="password" 
-                        className={mixins.inputField}
-                        type={showPassword ? "text" : "password"} 
-                        placeholder="Password" 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)}/>
-                    <button
-                        className={mixins.displayPasswordBtn}
-                        type="button"
-                        onClick={() => setShowPassword(prev => !prev)}>
+                    <div className={styles.passwordWrapper}>
+                        <input
+                            className={styles.input}
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+
+                        <button
+                            type="button"
+                            className={styles.passwordBtn}
+                            onClick={() => setShowPassword((prev) => !prev)}
+                        >
                             <img
-                                className={mixins.displayPasswordBtnIcon}
-                                src={showPassword ? "/eye-off.svg" : "/eye.svg"} 
-                                alt={showPassword ? "icon for displaying password" : "icon for hideing password"}  />
+                                src={showPassword ? "/eye-off.svg" : "/eye.svg"}
+                                alt="toggle password"
+                                className={styles.passwordIcon}
+                            />
+                        </button>
+                    </div>
+                </div>
+
+                {errors.general && (
+                    <p className={styles.error}>{errors.general}</p>
+                )}
+
+                {errors.locked && (
+                    <p className={styles.error}>{errors.locked}</p>
+                )}
+
+                <div className={styles.actions}>
+                    <button
+                        type="submit"
+                        className={styles.saveBtn}
+                        disabled={!username || !password}
+                    >
+                        Sign In
                     </button>
                 </div>
-            </div>
-
-            {/* UX MESSAGE */}
-            {errors.general 
-                ? <p className={mixins.errorMessage}>{errors.general}</p>
-                : <></>}
-
-            {errors.locked 
-                ? <p className={mixins.errorMessage}>{errors.locked}</p>
-                : <></>}
-
-            {/* SUBMIT BUTTON */}
-            <button
-                className={`${mixins.submitBtn} ${styles.submitBtn}`}
-                type="submit"
-                disabled={!username || !password}>
-                    Sign In
-            </button>
             </form>
 
-            <p className={styles.registerTitle}>Not a member?</p>
-            <Link to="/register" className={styles.register}>
-                Create an account
-            </Link>
+            <div className={styles.registerSection}>
+                <p className={styles.registerTitle}>Not a member?</p>
+
+                <Link
+                    to="/register"
+                    className={styles.register}
+                >
+                    Create an account
+                </Link>
+            </div>
         </section>
-        </>)}
-    </>
     );
-};
+}
