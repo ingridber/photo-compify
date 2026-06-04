745d5a2dd8a038b6350a1c69882c4f4f77f9e993
ollewarne <209520777+ollewarne@users.noreply.github.com>
2026-06-04 11:15:48 +0200

    remove react-router-dom since we use react-router

--- frontend/src/components/nav-bar/NotificationMenu.tsx
+++ frontend/src/components/nav-bar/NotificationMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import styles from './NotificationMenu.module.css'; 
 import { useNotifications } from '../../hooks/useNotifications';
 
