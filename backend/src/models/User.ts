a5770f9e33bd22d44dbbdfc3f6ee1179ca58650a
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-06-05 12:15:26 +0200

    put role in userSchema

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -6,6 +6,7 @@ const userSchema = new Schema<InterfaceUser>({
     username: { type: String, required: true, unique: true, lowercase: true, minlength: 3, maxlength: 80 },
     email: { type: String, required: false, lowercase: true, maxlength: 80 },
     password: { type: String, required: true, minlength: 8, maxlength: 128 },
+    role: { type: String, enum: ["user", "moderator", "admin"], default: "user", required: true },
     profilePicture: { type: Schema.Types.ObjectId, ref: "Image", default: null},
     warnings: { type: Number, default: 0 },
     loginAttempts: { type: Number, default: 0 },
