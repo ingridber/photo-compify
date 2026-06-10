5f0205bcf029e5817326b002bdc76fdecd47e2c6
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-06-10 16:23:29 +0200

    admin protect routes

--- backend/src/routes/report.ts
+++ backend/src/routes/report.ts
@@ -2,13 +2,14 @@ import express from "express";
 import { createReport, checkNoPreviousReport, getReports, reportUser, resolveReport } from "../controllers/reportController";
 import { reportRateLimit } from "../middleware/rateLimiter";
 import { authenticateToken } from "../middleware/auth";
+import { requireRole } from "../middleware/requireRole";
 
 const routerReport = express.Router();
 
 routerReport.post("/create", reportRateLimit, createReport);
-routerReport.post("/check", reportRateLimit, checkNoPreviousReport);
-routerReport.get("/", authenticateToken, getReports);
-routerReport.patch("/:id", authenticateToken, reportUser);
-routerReport.patch("/resolve/:id", authenticateToken, resolveReport);
+routerReport.post("/check", reportRateLimit, requireRole(["admin"]), checkNoPreviousReport);
+routerReport.get("/", authenticateToken, requireRole(["admin"]), getReports);
+routerReport.patch("/:id", authenticateToken, requireRole(["admin"]), reportUser);
+routerReport.patch("/resolve/:id", authenticateToken, requireRole(["admin"]), resolveReport);
 
 export {routerReport};
