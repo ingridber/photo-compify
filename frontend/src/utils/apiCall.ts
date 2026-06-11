1e861346c8707563ee4ba261766e9e06a6d0d5b6
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-06-11 16:16:48 +0200

    import report type, delete try, ctach

--- frontend/src/utils/apiCall.ts
+++ frontend/src/utils/apiCall.ts
@@ -1,17 +1,16 @@
+import type { ResolveReportData } from "../services/reportApi";
+
 type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-export async function apiCall(path: string, method: HttpMethod = "GET", body?: Record<string, unknown> | FormData): Promise<Response> {
+export async function apiCall(path: string, method: HttpMethod = "GET", body?: Record<string, unknown> | FormData | ResolveReportData): Promise<Response> {
     const BASE_URL = import.meta.env.VITE_API_URL;
     if (!BASE_URL) throw new Error("VITE_API_URL is not defined");
     const isFormData = body instanceof FormData;
     const headers: HeadersInit = isFormData ? {} : { "Content-Type": "application/json" };
-    try {
-        return await fetch(`${BASE_URL}${path}`, {
-            method: method,
-            credentials: "include",
-            headers: headers,
-            body: isFormData ? body : JSON.stringify(body)
-        });
-    } catch (error) {
-        throw error;
-    }
+
+    return await fetch(`${BASE_URL}${path}`, {
+        method: method,
+        credentials: "include",
+        headers: headers,
+        body: isFormData ? body : JSON.stringify(body)
+    });
 };
