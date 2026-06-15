d8d5d16400f8e184608a8cd7b75380f10c2dde50
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2026-06-15 13:32:46 +0200

    config fix

--- backend/src/middleware/uploadMiddleware.ts
+++ backend/src/middleware/uploadMiddleware.ts
@@ -1,8 +1,9 @@
 import multer from "multer";
+import { Request } from "express";
 
 const storage = multer.memoryStorage();
 
-const fileFilter = (_req: any, file: any, cb: any) => {
+const fileFilter = (_req: Request, file: any, cb: any) => {
   const allowedTypes = [ "image/jpeg", "image/png", "image/webp"];
 // "image/jpeg",
   if (allowedTypes.includes(file.mimetype)) {
